a216bab014334e49f634dcfa71693fa69ef6bea0
Shimon <shimi0007@hotmail.com>
2015-10-07 19:03:48 +0300

    API - WIP

--- routers.js
+++ routers.js
@@ -1,3 +1,32 @@
 /**
  * Created by Shimon on 07/10/2015.
  */
+var express = require('express');
+var routers = express.Router();
+var fs = require('fs');
+
+routers.get('/apartment', function(req, res){
+    var apartments = require('./apartments.json');
+    res.send(JSON.stringify(apartments));
+});
+
+routers.get('/apartments-list', function(req, res){
+    var apartments = require('./apartments.json');
+    apartments = apartments.items;
+    for(var i = 0 in apartments){
+        var image = {};
+        for(var j = 0 in apartments[i].images){
+            if(apartments[i].images[j].active){
+                apartments[i].image = apartments[i].images[j].path;
+                break;
+            }
+        }
+        delete apartments[i].images;
+        delete apartments[i].schedule;
+    }
+    res.send(apartments);
+});
+
+
+
+module.exports = routers;
